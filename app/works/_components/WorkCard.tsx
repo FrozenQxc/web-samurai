6801cf0d4ec624343ffbdb71dd97a2d4adb677a5
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-11-22 09:43:32 +0300

    hot-fix: card animate

--- app/works/_components/WorkCard.tsx
+++ app/works/_components/WorkCard.tsx
@@ -5,7 +5,7 @@ const WorkCard = () => {
 		<div className='bg-gradient-to-r from-gray-800 via-gray-900 to-black rounded-lg p-5 shadow-lg transform transition-all '>
 			<a href='https://react-pizza-nine-delta.vercel.app' className='group'>
 				<Image
-					className='rounded-lg transition-transform duration-300 animate-pulse'
+					className='rounded-lg transition-transform duration-300 hover:animate-pulse'
 					src='https://sun9-29.userapi.com/impg/hhEPCbtLH1w3Jn2ujU27P9sCWJIwIvW9hayxDw/KwsMsvOivvQ.jpg?size=1672x1025&quality=96&sign=db95cabbc7b0ff0fbcd357e7ab6b3a41&type=album'
 					width={500}
 					height={300}
